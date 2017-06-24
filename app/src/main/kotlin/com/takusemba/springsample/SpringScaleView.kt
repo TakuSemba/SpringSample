da9a3cb7d3e1ba70c6d45e5f395e5a5fbb54478e
takusemba <taku1008ele@gmail.com>
2017-06-25 00:49:13 +0900

    delete unwanted files, rename directory

--- app/src/main/kotlin/com/takusemba/springsample/SpringScaleView.kt
+++ app/src/main/kotlin/com/takusemba/springsample/SpringScaleView.kt
@@ -7,7 +7,6 @@ import android.util.AttributeSet
 import android.view.MotionEvent
 import android.view.ScaleGestureDetector
 import android.view.View
-import com.takusemba.springsample.extensions.*
 
 /**
  * Created by takusemba on 2017/06/24.
@@ -21,16 +20,19 @@ class SpringScaleView @JvmOverloads constructor(context: Context, attrs: Attribu
         val DAMPING_RATIO = SpringForce.DAMPING_RATIO_HIGH_BOUNCY
     }
 
-    private val xAnim: SpringAnimation = animWithProperty(SpringAnimation.SCALE_X)
-            .toPosition(INITIAL_SCALE)
-            .withStiffness(STIFFNESS)
-            .withDampingRatio(DAMPING_RATIO)
+    private val xAnim: SpringAnimation = SpringAnimation(this, SpringAnimation.SCALE_X).apply {
+        spring = SpringForce(INITIAL_SCALE).apply {
+            stiffness = STIFFNESS
+            dampingRatio = DAMPING_RATIO
+        }
+    }
 
-
-    private val yAnim: SpringAnimation = animWithProperty(SpringAnimation.SCALE_Y)
-            .toPosition(INITIAL_SCALE)
-            .withStiffness(STIFFNESS)
-            .withDampingRatio(DAMPING_RATIO)
+    private val yAnim: SpringAnimation = SpringAnimation(this, SpringAnimation.SCROLL_Y).apply {
+        spring = SpringForce(INITIAL_SCALE).apply {
+            stiffness = STIFFNESS
+            dampingRatio = DAMPING_RATIO
+        }
+    }
 
     private val detector = ScaleGestureDetector(context,
             object : ScaleGestureDetector.SimpleOnScaleGestureListener() {
